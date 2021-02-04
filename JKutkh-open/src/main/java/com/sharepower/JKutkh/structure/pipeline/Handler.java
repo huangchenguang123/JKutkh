11ab19c515f15a9cc81f6920c7d06e763cacc1c3
huangchenguang <huangchenguang@youzan.com>
2021-02-04 20:59:06 +0800

    change pom

--- JKutkh-open/src/main/java/com/sharepower/JKutkh/structure/pipeline/Handler.java
+++ JKutkh-open/src/main/java/com/sharepower/JKutkh/structure/pipeline/Handler.java
@@ -2,14 +2,11 @@ package com.sharepower.JKutkh.structure.pipeline;
 
 import java.util.Map;
 
-import lombok.Getter;
-
 /**
  * @date 2020/12/22
  * @author chenguang
  * @desc one block of pipeline
  */
-@Getter
 public abstract class Handler {
 
     /**
